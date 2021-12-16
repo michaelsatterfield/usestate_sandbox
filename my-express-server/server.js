60f5d647afd27eedf68524fb74a2b0141f4d5640
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-12-16 16:38:07 -0600

    addign get res and req to node server

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -6,6 +6,14 @@ const app = express();
 app.get('/', function (request, response){
     response.send("<h1>Hola Amigos/as</h1>")
 })
+
+app.get("/contact", function (req,res){
+    res.send("<h1>     Contact Us</h1>")
+})
+
+app.get("/about", function (req,res){
+    res.send("<h1>I'm Michael im a developer.</h1>")
+})
 app.listen(3001, function (){
     console.log("server started on port 3001")
 } )
