01c1c7f61c6fdc33c5b99ce0e65502963b52f51d
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-12-16 13:30:15 -0600

    added button function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
+import {useState} from "react";
+import './App.css'
+
 
 
 function App() {
-  return <div />;
+
+
+    const [count] = useState(123)
+
+        console.log(count);
+
+    let increase = () => {
+    }
+
+    return (
+        <div className="container">
+            <h1>{count}</h1>
+            <button onClick={increase}>+</button>
+        </div>
+
+    );
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import React from "react";
-import { useState} from "react";
 import ReactDOM from "react-dom";
-import './App.css'
+import './App'
+import App from "./App";
 
-
-let count = 0;
-
- let increase = () => {
-     console.log("button clicked!")
-}
 
 ReactDOM.render(
-    <div className="container">
-        <h1>{count}</h1>
-        <button onClick={increase}>+</button>
-    </div>,
+    <App/>,
     document.getElementById("root")
 );
