584857bce2ed2cfaa7240e6f807ddffa726fd4aa
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-12-16 12:25:11 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './App.css'
+
+ReactDOM.render(
+    <div className="container">
+        <h1>0</h1>
+        <button>+</button>
+    </div>,
+    document.getElementById("root")
+);
