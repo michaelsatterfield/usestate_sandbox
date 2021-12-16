80c1913a25a89538a649cb70aad9a6b6357bb896
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-12-16 13:09:36 -0600

    added button

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { useState} from "react";
 import ReactDOM from "react-dom";
 import './App.css'
 
+
+let count = 0;
+
+ let increase = () => {
+     console.log("button clicked!")
+}
+
 ReactDOM.render(
     <div className="container">
-        <h1>0</h1>
-        <button>+</button>
+        <h1>{count}</h1>
+        <button onClick={increase}>+</button>
     </div>,
     document.getElementById("root")
 );
